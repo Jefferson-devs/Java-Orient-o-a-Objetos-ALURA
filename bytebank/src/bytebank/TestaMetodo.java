5f14f472d756396c311680ba5952e50a36494a35
Jefferson-devs <jeffersonsoares.s@yahoo.com>
2021-10-04 11:35:57 -0300

    Continuação do curso da alura de POO

---
+++ bytebank/src/bytebank/TestaMetodo.java
@@ -0,0 +1,28 @@
+package bytebank;
+
+public class TestaMetodo {
+	public static void main(String[] args) {
+		Conta contaDoJefferson = new Conta();
+		contaDoJefferson.saldo = 100;
+		contaDoJefferson.deposita(50);
+		System.out.println(contaDoJefferson.saldo);
+		
+		boolean conseguiuRetirar = contaDoJefferson.saca(20);
+		System.out.println(contaDoJefferson.saldo);
+		System.out.println(conseguiuRetirar);
+		
+		Conta contaDaMarcela = new Conta();
+		contaDaMarcela.deposita(1000);
+		boolean sucessoTransferencia =
+				contaDaMarcela.transfere(3000, contaDoJefferson);
+		
+		if(sucessoTransferencia) {
+			System.out.println("transferencia com sucesso");
+		} else {
+			System.out.println("faltou dinheiro");
+		}
+		System.out.println(contaDaMarcela.saldo);
+		System.out.println(contaDoJefferson.saldo);
+	
+	}
+}
